0f1f68e3200da26b99a17481bbc27537ede0db0b
imellag <118837721+imellag@users.noreply.github.com>
2024-01-25 21:34:53 +0100

    Commit

--- app/src/main/java/com/example/fantalega/login/MainActivity.java
+++ app/src/main/java/com/example/fantalega/login/MainActivity.java
@@ -1,73 +1,35 @@
 package com.example.fantalega.login;
-import com.example.fantalega.HomeActivity;
-import com.example.fantalega.R;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fantalega.HomeActivity;
+import com.example.fantalega.R;
 
-    private EditText username, password;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        username = findViewById(R.id.loginUsername);
-        password = findViewById(R.id.loginPassword);
     }
 
-    public void onLoginClick(View view) {
-        navigateToHome();
-    }
-
+    public void onLoginClick(View view) {navigateToHome();}
     public void onRegisterClick(View view) {
         navigateToRegister();
     }
 
     private void navigateToHome() {
-        // Per ora controlla soltanto che le stringhe fornite non siano vuote
-        // Aggiungere controllo dei dati = account registrato
-        if (checkInput()) {
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void navigateToRegister() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
-
-    private boolean checkInput() {
-        int errors = 0;
-
-        String usernameText = username.getText().toString().trim();
-        if (TextUtils.isEmpty(usernameText)) {
-            errors++;
-            username.setError("Inserisci un nome!");
-        } else {
-            username.setError(null);
-        }
-
-        String passwordText = password.getText().toString().trim();
-        if (TextUtils.isEmpty(passwordText)) {
-            errors++;
-            password.setError("Inserisci una password!");
-        } else if (passwordText.length() < 6) {
-            errors++;
-            password.setError("La password deve avere almeno 6 caratteri!");
-        } else {
-            password.setError(null);
-        }
-
-        return errors == 0;
-    }
 }
-
--- app/src/main/java/com/example/fantalega/login/RegisterActivity.java
+++ app/src/main/java/com/example/fantalega/login/RegisterActivity.java
@@ -2,38 +2,25 @@ package com.example.fantalega.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fantalega.R;
 
-
 public class RegisterActivity extends AppCompatActivity {
-
-    private EditText username, email, password, confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        username = findViewById(R.id.registerUsername);
-        email = findViewById(R.id.registerEmail);
-        password = findViewById(R.id.registerPassword);
-        confirmPassword = findViewById(R.id.confirmPassword);
-
-        // Pulsante per confermare la creazione dell'account
         Button btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(v -> {
-            // Aggiungi la logica di registrazione
-            if (checkInput()) {
-                // Apri la schermata del login dopo aver creato l'account (aggiungere append)
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+                Toast.makeText(this, "Hai creato un nuovo account, ora puoi accedere!", Toast.LENGTH_SHORT).show();
+                navigateToLogin();
         });
     }
 
@@ -42,48 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void navigateToLogin() {
-        Intent intent = new Intent(this, MainActivity.class); // Modificato da RegisterActivity
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-
-    private boolean checkInput() {
-        int errors = 0;
-
-        String usernameText = username.getText().toString().trim();
-        if (TextUtils.isEmpty(usernameText)) {
-            errors++;
-            username.setError("Inserisci un nome!");
-        } else {
-            username.setError(null);
-        }
-
-        String emailText = email.getText().toString().trim();
-        if (TextUtils.isEmpty(emailText)) {
-            errors++;
-            email.setError("Inserisci un'email!");
-        } else {
-            email.setError(null);
-        }
-
-        String passwordText = password.getText().toString().trim();
-        if (TextUtils.isEmpty(passwordText)) {
-            errors++;
-            password.setError("Inserisci una password!");
-        } else if (passwordText.length() < 6) {
-            errors++;
-            password.setError("La password deve avere almeno 6 caratteri!");
-        } else {
-            password.setError(null);
-        }
-
-        String confirmPasswordText = confirmPassword.getText().toString().trim();
-        if (!passwordText.equals(confirmPasswordText)) {
-            errors++;
-            confirmPassword.setError("Le password non coincidono!");
-        } else {
-            confirmPassword.setError(null);
-        }
-
-        return errors == 0;
     }
 }

1fcce93db542dbf39e9830c5b82a98113787f7b0
imellag <118837721+imellag@users.noreply.github.com>
2024-02-11 15:22:09 +0100

    fix

--- app/src/main/java/com/example/fantalega/campionato/ChampionshipActivity.java
+++ app/src/main/java/com/example/fantalega/campionato/ChampionshipActivity.java
@@ -1,13 +1,10 @@
 package com.example.fantalega.campionato;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fantalega.R;
-import com.example.fantalega.ScegliGiocatoreActivity;
 
 public class ChampionshipActivity extends AppCompatActivity {
 
@@ -15,15 +12,5 @@ public class ChampionshipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_championship);
-    }
-
-    public void aggiungiCampionato(View view) {
-        Intent intent = new Intent(this, CreaCampionatoActivity.class);
-        startActivity(intent);
-    }
-
-    public void accediCampionato(View view) {
-        Intent intent = new Intent(this, AccediCampionatoActivity.class);
-        startActivity(intent);
     }
 }
